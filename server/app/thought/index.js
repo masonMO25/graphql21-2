0249095fb70fe05da83ac5b6488d0ad88ca791ea
Manav Misra <manavm@visionwebsoft.com>
2023-03-13 23:30:43 -0500

    fix: set up `createdAt` using `timestamps: true`

--- server/app/thought/index.js
+++ server/app/thought/index.js
@@ -2,26 +2,33 @@ import { Schema } from "mongoose";
 import formatDateTime from "../format-date-time.js";
 import CommentSchema from "./comment-schema.js";
 
-const ThoughtSchema = new Schema({
-  thoughtText: {
-    type: String,
-    required: "You need to leave a thought 💭!",
-    minlength: 1,
-    maxlength: 280,
-  },
-  thoughtAuthor: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    get(timestamp) {
-      return formatDateTime(timestamp);
+const ThoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: "You need to leave a thought 💭!",
+      minlength: 1,
+      maxlength: 280,
     },
+    thoughtAuthor: {
+      type: String,
+      required: "Gotta leave your name so people know who to thank!",
+      trim: true,
+    },
+    createdAt: {
+      type: Date,
+      get(timestamp) {
+        return formatDateTime(timestamp);
+      },
+    },
+    comments: [CommentSchema],
   },
-  comments: [CommentSchema],
-});
+  {
+    timestamps: true,
+    toJSON: {
+      getters: true,
+    },
+  }
+);
 
 export default ThoughtSchema;
