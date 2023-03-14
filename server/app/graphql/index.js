c7843e52112d41f42d606780d50ca11f99a56c4e
masonMO25 <masonneale@gmail.com>
2023-03-14 18:45:17 -0500

    added index

--- server/app/graphql/index.js
+++ server/app/graphql/index.js
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 export { default as resolvers } from "./resolvers.js";
 export { default as typeDefs } from "./type-defs.js";
-=======
-export { default as typeDefs } from "./type-defs.js";
-export { default as resolvers } from "./resolvers.js";
->>>>>>> 078efa907b923fd9606b37af4703d3fcd4d4921b
