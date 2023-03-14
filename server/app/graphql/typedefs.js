ace00adaa6168442d1113393d2cd78f3f16565b2
Manav Misra <manavm@visionwebsoft.com>
2023-03-13 23:05:59 -0500

    Add typeDefs for profiles

---
+++ server/app/graphql/typedefs.js
@@ -0,0 +1,30 @@
+import { gql } from "graphql-tag";
+
+const typeDefs = gql`
+  type Query {
+    "Get all profiles"
+    profiles: [Profile!]!
+    "Get a profile by id"
+    profile(id: ID!): Profile
+  }
+
+  type Mutation {
+    "Create a new profile"
+    createProfile(name: String!, skills: [String!]!): Profile!
+    "Add a skill to a profile"
+    addSkill2Profile(id: ID!, skill: String!): Profile!
+    "Remove a skill from a profile"
+    removeSkillFromProfile(id: ID!, skill: String!): Profile!
+    "Delete a profile"
+    deleteProfile(id: ID!): Profile!
+  }
+
+  # This is basically a duplicate of the Mongoose schema
+  type Profile {
+    id: ID!
+    "Full name of the person 🧑‍🦰"
+    name: String!
+    "A list of the person's programming skills 🧑‍💻"
+    skills: [String!]!
+  }
+`;
