e5b90c4ffac61a9a9eea312a0c964b0fcfffd50a
Manav Misra <manavm@visionwebsoft.com>
2023-03-14 21:28:06 -0500

    Add routes for Profile and Thoughts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,31 @@
-import Thoughts from "./routes/thoughts";
+import { Link, RouterProvider, createBrowserRouter } from "react-router-dom";
 import Profiles from "./routes/profiles";
+import Thoughts from "./routes/thoughts";
+
+const router = createBrowserRouter([
+  {
+    path: "/profiles",
+    element: <Profiles />,
+  },
+  {
+    path: "/thoughts",
+    element: <Thoughts />,
+  },
+]);
 
 export default function App() {
-  return <Profiles />;
+  return (
+    <RouterProvider router={router}>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/profiles">Profiles</Link>
+          </li>
+          <li>
+            <Link to="/thoughts">Thoughts</Link>
+          </li>
+        </ul>
+      </nav>
+    </RouterProvider>
+  );
 }
