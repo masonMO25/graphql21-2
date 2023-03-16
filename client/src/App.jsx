2180ca3723ba33845d2b220c1584c9b08faaa386
masonMO25 <masonneale@gmail.com>
2023-03-16 18:50:55 -0500

    update appjsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,17 @@
 import Thoughts from "./routes/thoughts";
+import Profiles from "./routes/profiles";
 
 export default function App() {
-  return <Thoughts />;
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="/profiles">Profiles</a>
+        </li>
+        <li>
+          <a href="/thoughts">Thoughts</a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
